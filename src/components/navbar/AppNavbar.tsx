e004c49826d7e10e0b906c2b9d9bc148ca14ab2a
greenaka0707 <produkpebeka@gmail.com>
2026-06-08 17:31:50 +0700

    feat: improve dashboard and mobile layout

--- src/components/navbar/AppNavbar.tsx
+++ src/components/navbar/AppNavbar.tsx
@@ -1,7 +1,9 @@
 export default function AppNavbar() {
   return (
-    <header className="flex h-16 items-center justify-between border-b px-6">
-      <h1 className="font-semibold">ERP Manufacture Kopi SaaS</h1>
+    <header className="flex h-16 items-center justify-between border-b bg-white px-6">
+      <h1 className="hidden md:block font-semibold">ERP Manufacture Kopi SaaS</h1>
+
+      <h1 className="block md:hidden ml-12 text-sm font-semibold">ERP Kopi</h1>
 
       <div>Admin</div>
     </header>
